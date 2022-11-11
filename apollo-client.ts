c893a5a6b67943ea5c019ee79c5d680948e4beb6
Michał Sukiennik <79942503+maykeloo@users.noreply.github.com>
2022-11-11 16:26:42 +0100

    apollo client change uri

--- apollo-client.ts
+++ apollo-client.ts
@@ -2,15 +2,12 @@ import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: "https://online-shop-esvg1zfpu-maykeloo.vercel.app//api/graphql",
+  uri: "/api/graphql",
 });
 //https://online-shop-at6xfo50e-maykeloo.vercel.app/api/graphql
 
 const authLink = setContext((_, { headers }) => {
-  let token;
-  if (typeof window !== 'undefined') {
-    token = localStorage.getItem("token");
-  }
+  const token = localStorage.getItem("token");
 
   return {
     headers: {
