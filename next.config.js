52b2deb007bde38619f6bc7f341466f7e62d7f46
Michał Sukiennik <79942503+maykeloo@users.noreply.github.com>
2022-11-11 16:15:20 +0100

    cors fix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://online-shop-85o1q5se3-maykeloo.vercel.app/:path*',
+      },
+    ]
+  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['naszsklep-api.vercel.app'],
+    domains: ['https://online-shop-85o1q5se3-maykeloo.vercel.app'],
     formats: ['image/avif', 'image/webp']
   }
 }
